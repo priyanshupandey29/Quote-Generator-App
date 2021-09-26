680f90cd58eb598d24918697f0a5f99dd1108a4a
priyanshupandey29 <priyanshupandey299@gmail.com>
2021-09-26 14:11:56 +0530

    Webapp

---
+++ script.js
@@ -0,0 +1,16 @@
+const box = document.querySelector(".box");
+const next = document.querySelector(".clickMe");
+const quotedata = document.querySelector(".quote");
+const authordata = document.querySelector(".author");
+
+next.addEventListener("click", () => {
+  fetch("https://api.quotable.io/random")
+    .then((res) => res.json())
+    .then((data) => {
+      box.style.display = "flex";
+      let author = data.author;
+      let quote = data.content;
+      quotedata.innerHTML = quote;
+      authordata.innerHTML = `By: ${author}`;
+    });
+});
